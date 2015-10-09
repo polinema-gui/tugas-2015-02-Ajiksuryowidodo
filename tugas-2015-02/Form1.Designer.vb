31b92f79ddcfa55afaa518785fe7066c998791a8
Ajiksuryowidodo <ajikpredator1922@gmail.com>
2015-10-09 11:22:56 +0700

    TUGAS 2 KALKULATOR

--- tugas-2015-02/Form1.Designer.vb
+++ tugas-2015-02/Form1.Designer.vb
@@ -22,9 +22,235 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.btnon = New System.Windows.Forms.Button()
+        Me.btnoff = New System.Windows.Forms.Button()
+        Me.btnkurang = New System.Windows.Forms.Button()
+        Me.btnjumlah = New System.Windows.Forms.Button()
+        Me.btntotal = New System.Windows.Forms.Button()
+        Me.Btn0 = New System.Windows.Forms.Button()
+        Me.btn3 = New System.Windows.Forms.Button()
+        Me.btn2 = New System.Windows.Forms.Button()
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.btn6 = New System.Windows.Forms.Button()
+        Me.btn5 = New System.Windows.Forms.Button()
+        Me.btn4 = New System.Windows.Forms.Button()
+        Me.btnhapus = New System.Windows.Forms.Button()
+        Me.btn9 = New System.Windows.Forms.Button()
+        Me.btn8 = New System.Windows.Forms.Button()
+        Me.Textlayar = New System.Windows.Forms.TextBox()
+        Me.btn7 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(58, 54)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(49, 36)
+        Me.Button1.TabIndex = 35
+        Me.Button1.Text = "exit"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'btnon
+        '
+        Me.btnon.Location = New System.Drawing.Point(110, 54)
+        Me.btnon.Name = "btnon"
+        Me.btnon.Size = New System.Drawing.Size(46, 36)
+        Me.btnon.TabIndex = 34
+        Me.btnon.Text = "ON"
+        Me.btnon.UseVisualStyleBackColor = True
+        '
+        'btnoff
+        '
+        Me.btnoff.Location = New System.Drawing.Point(162, 54)
+        Me.btnoff.Name = "btnoff"
+        Me.btnoff.Size = New System.Drawing.Size(46, 36)
+        Me.btnoff.TabIndex = 33
+        Me.btnoff.Text = "OFF"
+        Me.btnoff.UseVisualStyleBackColor = True
+        '
+        'btnkurang
+        '
+        Me.btnkurang.Location = New System.Drawing.Point(162, 175)
+        Me.btnkurang.Name = "btnkurang"
+        Me.btnkurang.Size = New System.Drawing.Size(46, 36)
+        Me.btnkurang.TabIndex = 32
+        Me.btnkurang.Text = "--"
+        Me.btnkurang.UseVisualStyleBackColor = True
+        '
+        'btnjumlah
+        '
+        Me.btnjumlah.Location = New System.Drawing.Point(162, 217)
+        Me.btnjumlah.Name = "btnjumlah"
+        Me.btnjumlah.Size = New System.Drawing.Size(46, 36)
+        Me.btnjumlah.TabIndex = 31
+        Me.btnjumlah.Text = "+"
+        Me.btnjumlah.UseVisualStyleBackColor = True
+        '
+        'btntotal
+        '
+        Me.btntotal.Location = New System.Drawing.Point(58, 217)
+        Me.btntotal.Name = "btntotal"
+        Me.btntotal.Size = New System.Drawing.Size(98, 36)
+        Me.btntotal.TabIndex = 30
+        Me.btntotal.Text = "="
+        Me.btntotal.UseVisualStyleBackColor = True
+        '
+        'Btn0
+        '
+        Me.Btn0.Location = New System.Drawing.Point(6, 217)
+        Me.Btn0.Name = "Btn0"
+        Me.Btn0.Size = New System.Drawing.Size(46, 36)
+        Me.Btn0.TabIndex = 29
+        Me.Btn0.Text = "0"
+        Me.Btn0.UseVisualStyleBackColor = True
+        '
+        'btn3
+        '
+        Me.btn3.Location = New System.Drawing.Point(110, 175)
+        Me.btn3.Name = "btn3"
+        Me.btn3.Size = New System.Drawing.Size(46, 36)
+        Me.btn3.TabIndex = 28
+        Me.btn3.Text = "3"
+        Me.btn3.UseVisualStyleBackColor = True
+        '
+        'btn2
+        '
+        Me.btn2.Location = New System.Drawing.Point(58, 175)
+        Me.btn2.Name = "btn2"
+        Me.btn2.Size = New System.Drawing.Size(46, 36)
+        Me.btn2.TabIndex = 27
+        Me.btn2.Text = "2"
+        Me.btn2.UseVisualStyleBackColor = True
+        '
+        'btn1
+        '
+        Me.btn1.Location = New System.Drawing.Point(6, 175)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(46, 36)
+        Me.btn1.TabIndex = 26
+        Me.btn1.Text = "1"
+        Me.btn1.UseVisualStyleBackColor = True
+        '
+        'btn6
+        '
+        Me.btn6.Location = New System.Drawing.Point(110, 133)
+        Me.btn6.Name = "btn6"
+        Me.btn6.Size = New System.Drawing.Size(46, 36)
+        Me.btn6.TabIndex = 25
+        Me.btn6.Text = "6"
+        Me.btn6.UseVisualStyleBackColor = True
+        '
+        'btn5
+        '
+        Me.btn5.Location = New System.Drawing.Point(58, 133)
+        Me.btn5.Name = "btn5"
+        Me.btn5.Size = New System.Drawing.Size(46, 36)
+        Me.btn5.TabIndex = 24
+        Me.btn5.Text = "5"
+        Me.btn5.UseVisualStyleBackColor = True
+        '
+        'btn4
+        '
+        Me.btn4.Location = New System.Drawing.Point(6, 133)
+        Me.btn4.Name = "btn4"
+        Me.btn4.Size = New System.Drawing.Size(46, 36)
+        Me.btn4.TabIndex = 23
+        Me.btn4.Text = "4"
+        Me.btn4.UseVisualStyleBackColor = True
+        '
+        'btnhapus
+        '
+        Me.btnhapus.Location = New System.Drawing.Point(162, 91)
+        Me.btnhapus.Name = "btnhapus"
+        Me.btnhapus.Size = New System.Drawing.Size(46, 78)
+        Me.btnhapus.TabIndex = 22
+        Me.btnhapus.Text = "clear"
+        Me.btnhapus.UseVisualStyleBackColor = True
+        '
+        'btn9
+        '
+        Me.btn9.Location = New System.Drawing.Point(110, 91)
+        Me.btn9.Name = "btn9"
+        Me.btn9.Size = New System.Drawing.Size(46, 36)
+        Me.btn9.TabIndex = 21
+        Me.btn9.Text = "9"
+        Me.btn9.UseVisualStyleBackColor = True
+        '
+        'btn8
+        '
+        Me.btn8.Location = New System.Drawing.Point(58, 91)
+        Me.btn8.Name = "btn8"
+        Me.btn8.Size = New System.Drawing.Size(46, 36)
+        Me.btn8.TabIndex = 20
+        Me.btn8.Text = "8"
+        Me.btn8.UseVisualStyleBackColor = True
+        '
+        'Textlayar
+        '
+        Me.Textlayar.Enabled = False
+        Me.Textlayar.Font = New System.Drawing.Font("Modern No. 20", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Textlayar.Location = New System.Drawing.Point(5, 15)
+        Me.Textlayar.Name = "Textlayar"
+        Me.Textlayar.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.Textlayar.Size = New System.Drawing.Size(203, 33)
+        Me.Textlayar.TabIndex = 19
+        '
+        'btn7
+        '
+        Me.btn7.Location = New System.Drawing.Point(6, 91)
+        Me.btn7.Name = "btn7"
+        Me.btn7.Size = New System.Drawing.Size(46, 36)
+        Me.btn7.TabIndex = 18
+        Me.btn7.Text = "7"
+        Me.btn7.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(233, 257)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.btnon)
+        Me.Controls.Add(Me.btnoff)
+        Me.Controls.Add(Me.btnkurang)
+        Me.Controls.Add(Me.btnjumlah)
+        Me.Controls.Add(Me.btntotal)
+        Me.Controls.Add(Me.Btn0)
+        Me.Controls.Add(Me.btn3)
+        Me.Controls.Add(Me.btn2)
+        Me.Controls.Add(Me.btn1)
+        Me.Controls.Add(Me.btn6)
+        Me.Controls.Add(Me.btn5)
+        Me.Controls.Add(Me.btn4)
+        Me.Controls.Add(Me.btnhapus)
+        Me.Controls.Add(Me.btn9)
+        Me.Controls.Add(Me.btn8)
+        Me.Controls.Add(Me.Textlayar)
+        Me.Controls.Add(Me.btn7)
+        Me.Name = "Form1"
+        Me.Text = "KALKULATOR AJIK"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents btnon As System.Windows.Forms.Button
+    Friend WithEvents btnoff As System.Windows.Forms.Button
+    Friend WithEvents btnkurang As System.Windows.Forms.Button
+    Friend WithEvents btnjumlah As System.Windows.Forms.Button
+    Friend WithEvents btntotal As System.Windows.Forms.Button
+    Friend WithEvents Btn0 As System.Windows.Forms.Button
+    Friend WithEvents btn3 As System.Windows.Forms.Button
+    Friend WithEvents btn2 As System.Windows.Forms.Button
+    Friend WithEvents btn1 As System.Windows.Forms.Button
+    Friend WithEvents btn6 As System.Windows.Forms.Button
+    Friend WithEvents btn5 As System.Windows.Forms.Button
+    Friend WithEvents btn4 As System.Windows.Forms.Button
+    Friend WithEvents btnhapus As System.Windows.Forms.Button
+    Friend WithEvents btn9 As System.Windows.Forms.Button
+    Friend WithEvents btn8 As System.Windows.Forms.Button
+    Friend WithEvents Textlayar As System.Windows.Forms.TextBox
+    Friend WithEvents btn7 As System.Windows.Forms.Button
 
 End Class
--- tugas-2015-02/Form1.vb
+++ tugas-2015-02/Form1.vb
@@ -1,3 +1,123 @@
 ﻿Public Class Form1
+    Dim nilai1, nilai2, hasil As Double
+    Dim a As String
 
+    Private Sub btnon_Click(sender As System.Object, e As System.EventArgs) Handles btnon.Click
+        Btn0.Enabled = True
+        btn1.Enabled = True
+        btn2.Enabled = True
+        btn3.Enabled = True
+        btn4.Enabled = True
+        btn5.Enabled = True
+        btn6.Enabled = True
+        btn7.Enabled = True
+        btn8.Enabled = True
+        btn9.Enabled = True
+        btnoff.Enabled = True
+        btnon.Enabled = False
+        Button1.Enabled = False
+    End Sub
+
+    Private Sub btnoff_Click(sender As System.Object, e As System.EventArgs) Handles btnoff.Click
+        Btn0.Enabled = False
+        btn1.Enabled = False
+        btn2.Enabled = False
+        btn3.Enabled = False
+        btn4.Enabled = False
+        btn5.Enabled = False
+        btn6.Enabled = False
+        btn7.Enabled = False
+        btn8.Enabled = False
+        btn9.Enabled = False
+        btnoff.Enabled = False
+        btnon.Enabled = True
+        Button1.Enabled = True
+    End Sub
+
+    Private Sub Btn0_Click(sender As System.Object, e As System.EventArgs) Handles Btn0.Click
+        Textlayar.Text = Textlayar.Text & "0"
+    End Sub
+
+    Private Sub btn1_Click(sender As System.Object, e As System.EventArgs) Handles btn1.Click
+        Textlayar.Text = Textlayar.Text & "1"
+    End Sub
+
+    Private Sub btn2_Click(sender As System.Object, e As System.EventArgs) Handles btn2.Click
+        Textlayar.Text = Textlayar.Text & "2"
+    End Sub
+
+    Private Sub btn3_Click(sender As System.Object, e As System.EventArgs) Handles btn3.Click
+        Textlayar.Text = Textlayar.Text & "3"
+    End Sub
+
+    Private Sub btn4_Click(sender As System.Object, e As System.EventArgs) Handles btn4.Click
+        Textlayar.Text = Textlayar.Text & "4"
+    End Sub
+
+    Private Sub btn5_Click(sender As System.Object, e As System.EventArgs) Handles btn5.Click
+        Textlayar.Text = Textlayar.Text & "5"
+    End Sub
+
+    Private Sub btn6_Click(sender As System.Object, e As System.EventArgs) Handles btn6.Click
+        Textlayar.Text = Textlayar.Text & "6"
+    End Sub
+
+    Private Sub btn7_Click(sender As System.Object, e As System.EventArgs) Handles btn7.Click
+        Textlayar.Text = Textlayar.Text & "7"
+    End Sub
+
+    Private Sub btn8_Click(sender As System.Object, e As System.EventArgs) Handles btn8.Click
+        Textlayar.Text = Textlayar.Text & "8"
+    End Sub
+
+    Private Sub btn9_Click(sender As System.Object, e As System.EventArgs) Handles btn9.Click
+        Textlayar.Text = Textlayar.Text & "9"
+    End Sub
+
+    Private Sub btnhapus_Click(sender As System.Object, e As System.EventArgs) Handles btnhapus.Click
+        Textlayar.Clear()
+    End Sub
+
+    Private Sub btntotal_Click(sender As System.Object, e As System.EventArgs) Handles btntotal.Click
+        If a = "+" Then
+            hasil = nilai1 + Val(Textlayar.Text)
+            Textlayar.Text = hasil
+        ElseIf a = "-" Then
+            hasil = nilai1 - Val(Textlayar.Text)
+            Textlayar.Text = hasil
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        If MsgBox("apa anda serius ingin keluar " + Textlayar.Text + "?", vbYesNo + vbQuestion, "ajik suryo widodo") = vbYes Then
+            End
+        End If
+    End Sub
+
+    Private Sub btnjumlah_Click(sender As System.Object, e As System.EventArgs) Handles btnjumlah.Click
+        nilai1 = Val(Textlayar.Text)
+        a = "+"
+        Textlayar.Text = ""
+    End Sub
+
+    Private Sub btnkurang_Click(sender As System.Object, e As System.EventArgs) Handles btnkurang.Click
+        nilai1 = Val(Textlayar.Text)
+        a = "-"
+        Textlayar.Text = ""
+    End Sub
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        Btn0.Enabled = False
+        btn1.Enabled = False
+        btn2.Enabled = False
+        btn3.Enabled = False
+        btn4.Enabled = False
+        btn5.Enabled = False
+        btn6.Enabled = False
+        btn7.Enabled = False
+        btn8.Enabled = False
+        btn9.Enabled = False
+        btnoff.Enabled = False
+        btnon.Enabled = True
+    End Sub
 End Class
